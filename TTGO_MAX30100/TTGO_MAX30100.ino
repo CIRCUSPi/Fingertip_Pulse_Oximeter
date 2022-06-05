3a52b31f3c731169969dac8d0ac63abc06e1d609
Zack Huang <icshop.edu5@gmail.com>
2022-06-05 13:13:01 +0800

    first commit

---
+++ TTGO_MAX30100/TTGO_MAX30100.ino
@@ -0,0 +1,386 @@
+/*
+ * @Author : Zack Huang
+ * @Link :
+ * @Date : 6/5/2022, 12:29:47 PM
+ *
+ * Hardware Used
+ * Development board: TTGO T-Display >> https://www.icshop.com.tw/product-page.php?28572
+ * Pulse Oximeter Sensor: MAX30100 >> https://www.icshop.com.tw/product-page.php?28034
+ * LiPo Battery: Lipo 500mAh 3.7V 602535 >> https://www.icshop.com.tw/product-page.php?14047
+ *
+ * Board Config: ESP32 Dev Module
+ *
+ * Librarys
+ *
+ * Arduino Library Manager:
+ * LittleFS_esp32(V1.0.6)       >> https://github.com/lorol/LITTLEFS
+ * MAX30100lib(V1.2.1)          >> https://github.com/oxullo/Arduino-MAX30100
+ * TJpg_Decoder(V1.0.5)         >> https://github.com/Bodmer/TJpg_Decoder
+ *
+ * Github
+ * 18650CL(e1be2aa)             >> https://github.com/pangodream/18650CL
+ * TFT_eSPI(4ced37a)            >> https://github.com/Bodmer/TFT_eSPI
+ * U8g2_for_TFT_eSPI(a170ef8)   >> https://github.com/Bodmer/U8g2_for_TFT_eSPI
+ *
+ * Notice: The MAX30100 uses high-speed(400k) I2C,
+ * and the distance of the transmission line
+ * should be kept within 10 cm to avoid transmission failure.
+ */
+
+#include "config.h"
+
+#include <SPI.h>
+#include <Wire.h>
+
+#include <LITTLEFS.h>
+#include <MAX30100_PulseOximeter.h>
+#include <Pangodream_18650_CL.h>
+#include <TFT_eSPI.h>
+#include <TJpg_Decoder.h>
+#include <U8g2_for_TFT_eSPI.h>
+
+/* #region Battery object and global var */
+Pangodream_18650_CL BL(ADC_PIN, CONV_FACTOR, READS);
+String              batteryImages[] = {"/hori_battery_01.jpg", "/hori_battery_02.jpg", "/hori_battery_03.jpg", "/hori_battery_04.jpg", "/hori_battery_05.jpg"};
+/* #endregion */
+
+/* #region  Max30100 object and global var */
+PulseOximeter pox;
+uint8_t       heart_rate_buff[HEART_RATE_BUFF_SIZE] = {0};
+uint8_t       spo2                                  = 0;
+uint8_t       heart_rate                            = 0;
+bool          detect_beat                           = false;
+bool          show_chart                            = false;
+int32_t       chart_num                             = 0;
+uint32_t      chart_clear_timer                     = 0;
+/* #endregion */
+
+/* #region  TFT object and global var */
+TFT_eSPI          tft   = TFT_eSPI();
+TFT_eSprite       graph = TFT_eSprite(&tft);
+U8g2_for_TFT_eSPI u8g2;
+int32_t           chartNumList[CHART_BUFF_SIZE] = {-1};
+/* #endregion */
+
+/* #region  Function Declaration */
+void SPIFFSInit(void);
+void TFTLibsInit(void);
+void MAX30100Init(void);
+void GPIOInit(void);
+void TaskMax30100(void);
+void TaskDisplay(void);
+void drawBatIcon(String filePath);
+bool onTJpgDecoded(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap);
+void drawTFTchart(int32_t *myNumList, byte chartType, bool dirType, uint16_t myColor);
+void clearTFTchartBuff();
+void onBeatDetected();
+/* #endregion */
+
+/* #region  Arduino Setup */
+void setup()
+{
+#if DEBUG_MODE
+    debugSerial.begin(115200);
+#endif
+    GPIOInit();
+    TFTLibsInit();
+    MAX30100Init();
+    SPIFFSInit();
+}
+/* #endregion */
+
+/* #region  SPIFFS Initialization */
+void SPIFFSInit(void)
+{
+    if (!SPIFFS.begin()) {
+        DEBUG_PRINTLN(F("SPIFFS initialisation failed!"));
+        while (1)
+            yield();
+    }
+    DEBUG_PRINTLN(F("\r\nInitialisation done."));
+}
+/* #endregion */
+
+/* #region  TFT Librars Initialization */
+void TFTLibsInit(void)
+{
+    // TFT
+    tft.init();
+    tft.fillScreen(TFT_BLACK);
+    tft.setRotation(3);
+    tft.setSwapBytes(true);
+    // u8g2
+    u8g2.begin(tft);
+    u8g2.setForegroundColor(TFT_WHITE);
+    // TJpgDec
+    TJpgDec.setJpgScale(1);
+    TJpgDec.setCallback(onTJpgDecoded);
+    // other
+    clearTFTchartBuff();
+}
+/* #endregion */
+
+/* #region  Max30100 Initialization */
+void MAX30100Init(void)
+{
+    // TODO: 加入開機畫面，版本資訊
+    tft.setCursor(0, 0, 4);
+    if (!pox.begin()) {
+        DEBUG_PRINTLN(F("FAILED"));
+        tft.print(F("FAILED"));
+        while (true)
+            yield();
+    } else {
+        DEBUG_PRINTLN(F("SUCCESS"));
+        tft.print(F("SUCCESS"));
+    }
+    // LED Configuration
+    pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+    // Register a callback for the beat detection
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+
+    // show initialization surface
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(10, 0, 4);
+    tft.setTextColor(TFT_ORANGE, TFT_BLACK);
+    tft.print("%SpO2");
+    tft.setCursor(153, 38, 4);
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.print("PRbpm");
+}
+/* #endregion */
+
+/* #region  GPIO Initialization */
+void GPIOInit(void)
+{
+    // pinMode(14, OUTPUT);
+    // digitalWrite(14, HIGH);
+}
+/* #endregion */
+
+/* #region  Arduino Loop */
+void loop()
+{
+    TaskMax30100();
+    TaskDisplay();
+}
+/* #endregion */
+
+/* #region  Task Max30100 */
+void TaskMax30100(void)
+{
+    static uint32_t sensor_get_timer            = 0;
+    static uint32_t pulse_get_timer             = 0;
+    static uint8_t  hr_buf_idx                  = 0;
+    static float    pulse_buff[PULSE_BUFF_SIZE] = {0.0f};
+    static uint8_t  pls_buf_idx                 = 0;
+
+    // update pulse oximeter
+    pox.update();
+
+    if (millis() > sensor_get_timer) {
+        // Get HeartRate and SpO2 Data
+        sensor_get_timer              = millis() + SENSOR_GET_TIMER_MS;
+        heart_rate_buff[hr_buf_idx++] = (uint8_t)pox.getHeartRate();
+        spo2                          = pox.getSpO2();
+        // Check buff index
+        if (hr_buf_idx >= HEART_RATE_BUFF_SIZE)
+            hr_buf_idx = 0;
+        // Calc avg heart rate
+        uint16_t avg_hr = 0;
+        for (uint8_t i = 0; i < HEART_RATE_BUFF_SIZE; i++)
+            avg_hr += heart_rate_buff[i];
+        heart_rate = (uint8_t)(avg_hr / HEART_RATE_BUFF_SIZE);
+    }
+
+    if (millis() > pulse_get_timer) {
+        pulse_get_timer = millis() + PULSE_GET_TIMER_MS;
+        // Get sensor filtered Pulse Value
+        pulse_buff[pls_buf_idx++] = pox.ftPlsVal;
+        // Check buff index
+        if (pls_buf_idx >= PULSE_BUFF_SIZE)
+            pls_buf_idx = 0;
+
+        float   min_avg = 0.0f, max_avg = 0.0f;
+        uint8_t min_num = 0, max_num = 0;
+        for (int i = 0; i < PULSE_BUFF_SIZE; i++) {
+            // total min pulse value
+            if (pulse_buff[i] < 0) {
+                min_avg += pulse_buff[i];
+                min_num++;
+            }
+            // total max pulse value
+            if (pulse_buff[i] > 0) {
+                max_avg += pulse_buff[i];
+                max_num++;
+            }
+        }
+        // Calc average
+        min_avg /= min_num;
+        max_avg /= max_num;
+        // Generate chart values
+        if (millis() > chart_clear_timer)
+            clearTFTchartBuff();
+        else
+            // FIXME: 重新校正?
+            chart_num = (int32_t)map(pox.ftPlsVal, min_avg, max_avg, 10, 0);
+        show_chart = true;
+    }
+}
+/* #endregion */
+
+/* #region  Task TFT Display */
+void TaskDisplay(void)
+{
+    static uint32_t show_bat_icon_timer  = 0;
+    static uint32_t show_beat_spo2_timer = 0;
+    static uint8_t  icon_idx             = 0;
+    char            str_buff[8];
+
+    // Display heartbeat and SpO2 at fixed time or when a heartbeat is detected
+    if (detect_beat || millis() > show_beat_spo2_timer) {
+        show_beat_spo2_timer = millis() + SHOW_SENSOR_DATA_TIMER_MS;
+        detect_beat          = false;
+        // Show HeartRate
+        sprintf(str_buff, "%03d", GET_MIN(heart_rate, 199));
+        tft.setCursor(153, 67);
+        tft.setTextSize(1);
+        tft.setTextFont(6);
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        tft.print(str_buff);
+        // Show SPO2
+        sprintf(str_buff, "%02d", GET_MIN(spo2, 99));
+        tft.setCursor(0, 30);
+        tft.setTextSize(1);
+        tft.setTextFont(8);
+        tft.setTextColor(TFT_ORANGE, TFT_BLACK);
+        tft.print(str_buff);
+    }
+
+    // show battery icon
+    if (BL.getBatteryVolts() >= MIN_USB_VOL) {
+        // Charge
+        if (millis() > show_bat_icon_timer) {
+            show_bat_icon_timer = millis() + CHRG_BAT_ICON_TIMER_MS;
+            // Draw Bat Icon
+            drawBatIcon(batteryImages[icon_idx++]);
+        }
+        // check bat icon idx
+        if (icon_idx >= ARRAY_SIZE(batteryImages))
+            icon_idx = 0;
+        tft.setCursor(110, 7);
+        tft.setTextSize(1);
+        tft.setTextFont(4);
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        tft.print(F("Chrg"));
+    } else {
+        // Discharge
+        if (millis() > show_bat_icon_timer) {
+            show_bat_icon_timer = millis() + DISCHRG_BAT_ICON_TIMER_MS;
+            int imgNum          = 0;
+            int batteryLevel    = BL.getBatteryChargeLevel();
+            if (batteryLevel >= 80)
+                imgNum = 3;
+            else if (batteryLevel < 80 && batteryLevel >= 50)
+                imgNum = 2;
+            else if (batteryLevel < 50 && batteryLevel >= 20)
+                imgNum = 1;
+            else if (batteryLevel < 20)
+                imgNum = 0;
+            drawBatIcon(batteryImages[imgNum]);
+            // TODO: 避免覆蓋
+            sprintf(str_buff, "%3d%%", GET_MIN(batteryLevel, 100));
+            tft.setCursor(105, 7);
+            tft.setTextSize(1);
+            tft.setTextFont(4);
+            tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
+            // Show battery Level
+            tft.print(str_buff);
+        }
+    }
+
+    // Show heart rate bar graph
+    if (show_chart) {
+        show_chart                    = false;
+        chartNumList[tft.width() - 1] = (int32_t)(chart_num + 10);
+        graph.createSprite(tft.width(), 30);
+        drawTFTchart(chartNumList, 1, false, tft.color565(0, 0, 255));
+        graph.pushSprite(0, 105);
+        graph.deleteSprite();
+    }
+
+    DEBUG_PRINT(F("HR:   "));
+    DEBUG_PRINT(heart_rate);
+    DEBUG_PRINT(F(" , SPO2: "));
+    DEBUG_PRINTLN(spo2);
+}
+/* #endregion */
+
+/* #region  Draw Battery Icon */
+void drawBatIcon(String filePath)
+{
+    TJpgDec.drawFsJpg(ICON_POS_X, 0, filePath);
+}
+/* #endregion */
+
+/* #region  Tjpg decode callback  */
+bool onTJpgDecoded(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap)
+{
+    if (y >= tft.height())
+        return 0;
+    // FIXME: Use DMA ?
+    tft.pushImage(x, y, w, h, bitmap);
+    return 1;
+}
+/* #endregion */
+
+/* #region  Draw TFT chart */
+/*
+ * myNumList >> 顯示buff，此方法將會移動buff內容
+ * chartType >> 顯示類型，0=折線圖，1=長條圖
+ * dirType   >> 刷新方向，true=左到右，false=右到左
+ * myColor   >> 顯示顏色
+ */
+void drawTFTchart(int32_t *myNumList, byte chartType, bool dirType, uint16_t myColor)
+{
+    byte myWidth = tft.width(), myHeight = tft.height();
+    graph.fillSprite(TFT_BLACK);
+    for (int i = 0; i < ((chartType == 0) ? (myWidth - 1) : myWidth); i++) {
+        if ((myNumList[i]) > -1 && (myNumList[i + 1]) > -1) {
+            switch (chartType) {
+            case 0:
+                if (!dirType)
+                    graph.drawLine(i, myNumList[i], i + 1, myNumList[i + 1], myColor);
+                else
+                    graph.drawLine(myWidth - 1 - i, myNumList[i], myWidth - 2 - i, myNumList[i + 1], myColor);
+                break;
+            case 1:
+                if (!dirType)
+                    graph.drawLine(i, myHeight - 1, i, myNumList[i], myColor);
+                else
+                    graph.drawLine(myWidth - 1 - i, myHeight - 1, myWidth - 1 - i, myNumList[i], myColor);
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < (myWidth - 1); i++) {
+        myNumList[i] = (myNumList[i + 1]);
+    }
+}
+/* #endregion */
+
+/* #region  Clear heart rate graph buffer */
+void clearTFTchartBuff()
+{
+    for (int i = 0; i < CHART_BUFF_SIZE; i++)
+        chartNumList[i] = -1;
+}
+/* #endregion */
+
+/* #region  Callback fired when a pulse is detected */
+void onBeatDetected()
+{
+    chart_clear_timer = millis() + CHART_CLEAR_TIMER_MS;
+    detect_beat       = true;
+}
+/* #endregion */
---
+++ TTGO_MAX30100/config.h
@@ -0,0 +1,47 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+/* #region  Development Settings */
+#define DEBUG_MODE false
+#if DEBUG_MODE
+#define debugSerial      Serial
+#define DEBUG_PRINT(x)   debugSerial.print(x)
+#define DEBUG_PRINTLN(x) debugSerial.println(x)
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#endif
+/* #endregion */
+
+/* #region  Timer */
+#define SENSOR_GET_TIMER_MS       800
+#define PULSE_GET_TIMER_MS        20
+#define CHART_CLEAR_TIMER_MS      2000
+#define CHRG_BAT_ICON_TIMER_MS    500
+#define DISCHRG_BAT_ICON_TIMER_MS 1000
+#define SHOW_SENSOR_DATA_TIMER_MS 1000
+/* #endregion */
+
+/* #region  Buffer Size */
+#define HEART_RATE_BUFF_SIZE 10
+#define PULSE_BUFF_SIZE      50
+#define CHART_BUFF_SIZE      240
+/* #endregion */
+
+/* #region  Utility */
+#define GET_MIN(a, b) ((a) > (b) ? (b) : (a))
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+/* #endregion */
+
+/* #region  Battery Config */
+#define ICON_WIDTH        70
+#define ICON_HEIGHT       36
+#define STATUS_HEIGHT_BAR ICON_HEIGHT
+#define ICON_POS_X        (tft.width() - ICON_WIDTH)
+#define MIN_USB_VOL       4.7
+#define ADC_PIN           34
+#define CONV_FACTOR       1.8
+#define READS             20
+/* #endregion */
+
+#endif
